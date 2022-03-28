fa03c10b959718dc45f6101c70405c2d9531f695
Mariagoretti05 <mgmkimani@gmail.com>
2022-03-28 11:36:57 -0700

    pointers to pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,25 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
- * @s: memory space to be filled
- * @b: byte to fill with
- * @n: number of spaces to fill
- *
- * Return: pointer to memory area s
+ * _memset - fill n bytes of memory with a constant value
+ * @s: pointer to memory area
+ * @b: constant value
+ * @n: number of bytes to fill
+ * Return: memory area
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	int size = n; /* only accept positive sizes */
+
+	if (size > 0)
 	{
-		*(s + i) = b;
+		int i;
+
+		for (i = 0; i < size; i++)
+			s[i] = b;
 	}
+
 	return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,25 @@
 #include "main.h"
 
 /**
- * _memcpy - copies memory area
- * @dest: destination of copy
- * @src: memory area to copy
- * @n: number of bytes to copy
- *
+ * _memcpy - copy n bytes of memory from source to destination
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
  * Return: pointer to dest
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	int size = n; /* only accept positive sizes */
+
+	if (size > 0)
 	{
-		*(dest + i) = *(src + i);
+		int i;
+
+		for (i = 0; i < size; i++)
+			dest[i] = src[i];
 	}
+
 	return (dest);
 }
