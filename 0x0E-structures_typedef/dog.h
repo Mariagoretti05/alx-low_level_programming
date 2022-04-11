3c2049e6e1abf71b94e0dc4897e3ef40dde85c0c
Mariagoretti05 <mgmkimani@gmail.com>
2022-04-11 09:20:36 -0700

    preprocessors

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 /**
- * struct dog - A new type describing a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * struct dog - structure for dogs
+ * @name: first member
+ * @age: second member
+ * @owner: third member
  */
 struct dog
 {
@@ -14,11 +18,6 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
