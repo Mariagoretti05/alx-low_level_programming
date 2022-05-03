5ab0f5f47cb0d9762686b6b152e1963b6f0a429f
Mariagoretti05 <mgmkimani@gmail.com>
2022-05-03 08:38:48 -0700

    add 0 read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,51 +1,35 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of text file
  * @letters: number of letters it should read and print
- *
- * Return: actual number of letters it could read and print. Otherwise 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buf;
-	ssize_t rcount, wcount;
+	ssize_t o, r, w;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	/* open file */
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	/* create buf */
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		close(fd);
+		free(buffer);
 		return (0);
 	}
 
-	/* read file */
-	rcount = read(fd, buf, letters);
-	if (rcount == -1)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+	free(buffer);
+	close(o);
 
-	close(fd);
-
-	/* write to std_out */
-	wcount = write(STDOUT_FILENO, buf, rcount);
-	if (wcount == -1)
-	{
-		free(buf);
-		return (0);
-	}
-	free(buf);
-	return (wcount);
+	return (w);
+}
